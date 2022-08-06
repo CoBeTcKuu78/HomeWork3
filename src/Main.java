360a005cacb7cf83a1b411a6c2968fc4a1078c3b
CoBeTcKuu <9437855@gmail.com>
2022-08-06 16:48:26 +0300

    коммит задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        // задание 1
-        int clientOS = 0;
+        // задание 2
+        int clientOS = 1;
         if (clientOS == 1) {
             System.out.println("Установите приложение для Android");
         } else {
             System.out.println("Установите приложение для iOS");
+        }
+        int clientDeviceYear = 2017;
+        if (clientDeviceYear < 2015) {
+            System.out.println("Установите облегчённую версию приложения");
+        //}else{
+            //System.out.println("Установите приложение для Android");
         }
     }
 }
