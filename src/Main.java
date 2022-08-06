833fac4c0e57afe767c862c8cf7d0f8504d8a0e0
CoBeTcKuu <9437855@gmail.com>
2022-08-06 16:28:16 +0300

    коммит задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // задание 1
+        int clientOS = 0;
+        if (clientOS == 1) {
+            System.out.println("Установите приложение для Android");
+        } else {
+            System.out.println("Установите приложение для iOS");
+        }
     }
 }
